f58df10e13f9adbd60f0482885c6d627af3c7db2
Abednego Bogi Christian <99196129+AbogiC@users.noreply.github.com>
2022-04-24 12:42:56 +0700

    revisi foreign key

--- src/main/java/com/database/perpus/model/entity/Entity4DataPerpus.java
+++ src/main/java/com/database/perpus/model/entity/Entity4DataPerpus.java
@@ -1,9 +1,6 @@
 package com.database.perpus.model.entity;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "t_rak")
